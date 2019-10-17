d235ee04a1668ddc9ef8150e0f702f461533375f
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2019-10-17 10:50:43 -0400

    Updated active deck for activeDeck being an object

--- src/pages/active-deck.js
+++ src/pages/active-deck.js
@@ -4,7 +4,9 @@ import { Flex } from '@chakra-ui/core'
 
 const ActiveDeckPage = ({ activeDeck }) => (
 	<Flex direction="column" h="90vh" border="1px solid blue">
-		<h1>New deck: {activeDeck}</h1>
+		<h1>
+			New deck: {activeDeck.name} -- {activeDeck.class}
+		</h1>
 
 		<Link to="/home">Go home</Link>
 	</Flex>
