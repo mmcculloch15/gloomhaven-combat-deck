ae8e9d3093fe30627bcac849b76fb83c9952ee9d
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2019-10-20 12:58:51 -0400

    Added name input to character overview.  Started using some shorthands for commonly used style props

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from './Link'
 
-const CTA = ({ children, to, onClick, marginTop }) => (
+const CTA = ({ children, to, onClick, mt }) => (
 	<Link
 		bg="green.400"
 		width="100%"
@@ -13,7 +13,7 @@ const CTA = ({ children, to, onClick, marginTop }) => (
 		border="1px solid black"
 		to={to}
 		onClick={onClick}
-		marginTop={marginTop}
+		mt={mt}
 	>
 		{children}
 	</Link>
--- src/pages/character-overview/character-overview.js
+++ src/pages/character-overview/character-overview.js
@@ -1,30 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import characters from '../../data/characters.data'
 import Perk from './Perk'
-import { Flex, Icon, Heading } from '@chakra-ui/core'
+import { Flex, Icon, Heading, Input, Text } from '@chakra-ui/core'
 import { capitalize } from '../../utils/helpers'
 import CTA from '../../components/CTA'
 
 const CharacterOverviewPage = ({ match, setActiveDeck }) => {
+	const [deckName, setDeckName] = useState('')
+	const handleChange = event => setDeckName(event.target.value)
 	const character = characters.find(character => character.class === match.params.class)
 
 	return (
-		<Flex direction="column" padding="0.5rem">
-			<Flex marginBottom="2rem">
-				<Icon name={character.class} size="32px" marginRight="0.5rem" />
-				<Heading as="h1">{capitalize(character.class)}</Heading>
+		<Flex direction="column" p="0.5rem">
+			<Flex mb="2rem">
+				<Icon name={character.class} size="48px" mr="1rem" />
+				<Heading as="h1" fontSize="40px">
+					{capitalize(character.class)}
+				</Heading>
 			</Flex>
-			<Heading as="h2" fontSize="xl" marginBottom="0.5rem">
+			<Text as="label" fontSize="xl" fontWeight="bold" mb="0.25rem">
+				Character Name
+			</Text>
+			<Input variant="outline" value={deckName} onChange={handleChange} mb="1rem" />
+			<Heading as="h2" fontSize="xl" mb="0.5rem">
 				Perks
 			</Heading>
 			{character.perks.map((perk, i) => (
 				<Perk count={perk.count} name={perk.name} key={`perk${i}`} />
 			))}
-			<CTA
-				to="/deck"
-				onClick={() => setActiveDeck({ name: 'Test name', class: character.class })}
-				marginTop="3rem"
-			>
+			<CTA to="/deck" onClick={() => setActiveDeck({ name: deckName, class: character.class })} mt="3rem">
 				Start new deck
 			</CTA>
 		</Flex>
