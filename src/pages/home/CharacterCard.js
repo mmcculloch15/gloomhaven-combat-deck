254fe4038d313ad5bca6a96a7479e07e29cc14e9
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2019-10-23 18:31:01 -0400

    Add soem temporary hardcoded data for eased testing of deck page

--- src/pages/home/CharacterCard.js
+++ src/pages/home/CharacterCard.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Flex, PseudoBox, Text, Icon } from '@chakra-ui/core'
 import Link from '../../components/Link'
 import { capitalize } from '../../utils/helpers'
+import BASE_COMBAT_DECK from '../../data/base-combat-deck.data' //TODO: remove this when no longer necessary
 
 //TODO: Clean up the naming of characters/classes/display names.  It's pretty verbose/annoying/not even clear
 //Look up what Gloomhaven calls them
 //Could just expect an object that combines the user specified name all the other initial character data we will need anyway
 const CharacterCard = ({ name, characterClass, setActiveDeck }, ...props) => {
-	const handleClick = () => setActiveDeck({ name: name, class: characterClass })
+	const handleClick = () => setActiveDeck({ name: name, class: characterClass, cards: BASE_COMBAT_DECK })
 
 	return (
 		<Link to="/deck" onClick={handleClick}>
