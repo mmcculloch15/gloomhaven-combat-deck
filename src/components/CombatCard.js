2d5438826db836584beb78b383fe2d5652990dc7
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2019-10-23 18:31:15 -0400

    Initial CombatCard

---
+++ src/components/CombatCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Flex, Badge } from '@chakra-ui/core'
+
+const CombatCard = ({ type, count }) => (
+	<Flex direction="column" w="100%" h="100px" border="1px solid black" borderRadius="8px">
+		{count > 1 ? (
+			<Badge
+				variant="solid"
+				variantColor="green"
+				ml="60%"
+				mt="70px"
+				width="80px"
+				height="20px"
+				textAlign="center"
+				border="1px 0px 1px 1px solid black"
+				alignSelf="flex-end"
+			>
+				{count} copies
+			</Badge>
+		) : null}
+	</Flex>
+)
+
+export default CombatCard
