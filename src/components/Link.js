eb65e48986a36f09654c4944e90bfce347806031
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2020-01-24 09:57:39 -0500

    Link proptypes

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link as ChakraLink } from '@chakra-ui/core'
 import { Link as RouterLink } from 'react-router-dom'
 
-const Link = props => (
-	<ChakraLink {...props} as={RouterLink}>
-		{props.children}
-	</ChakraLink>
+const Link = ({ children, to }) => (
+  <ChakraLink to={to} as={RouterLink}>
+    {children}
+  </ChakraLink>
 )
+
+Link.propTypes = {
+  children: PropTypes.any,
+  to: PropTypes.string,
+}
 
 export default Link
