a904eab7ff15f788e4d951c8ce3a2bfc751e8b41
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2020-01-30 20:16:41 -0500

    Perk WIP

--- src/pages/deck/PerkGroup.js
+++ src/pages/deck/PerkGroup.js
@@ -6,7 +6,7 @@ import { Checkbox, CheckboxGroup } from '@chakra-ui/core'
 const PerkGroup = ({ perks, onChange }) => (
   <>
     {perks.map(perk => (
-      <Checkbox size="lg" name={perk.name} onChange={onChange}>
+      <Checkbox size="lg" name={perk.name} value={perk.value} onChange={onChange}>
         {perk.name}
       </Checkbox>
     ))}
